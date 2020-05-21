886e32fc81c681a46e83066ac7675e4d816526dc
Alexander Islas <alexanderislas8@gmail.com>
2020-05-20 20:38:37 -0700

    Update collection.py
    Added argument parser to address criteria in project. Has job and filename inputs.
    Iterates through input file and will pass to the request function to be made.

--- collection.py
+++ collection.py
@@ -8,24 +8,45 @@ Code to request and download data for use in Ocean 506
 try:
     import os, sys
     import argparse
+    # Do we really want to use this? 
+    sys.path.append(os.path.abspath('shared'))
+    import directory
 except ImportError:
     print(ImportError)
     sys.exit()
 
 # Directory management
 main_dir = os.path.abspath('.').split('/')[-1]
-input_dir = f"../{main_dir}_input"
-output_dir = f"../{main_dir}_output"
-
+input_dir = f"../{main_dir}_input/"
+output_dir = f"../{main_dir}_output/"
+directory.make_dir(input_dir)
+directory.make_dir(output_dir)
 
 
 # Argument Parser
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--filename', default='null', type=str)
+parser = argparse.ArgumentParser(description="Collector of data from the Ocean Gene Database")
+parser.add_argument('-f', '--filename',  type=str,
+                    help="Input filename to iterate sequences with")
 args = parser.parse_args()
 
-if args.filename == 'null':
-    print("You need to input the sequences into a textfile.")
-else:
-    # Run code to read jobs from text file.
-    pass
+# Check to make sure file exists and parse inputs.
+try:
+    f = open(args.filename)
+    lines = f.readlines()
+    sequences = []
+    sequence = ""
+    for line in lines:
+        if "END OF SEQUENCE" in line:
+            sequences.append(sequence)
+            sequence = ""
+        else:
+            sequence = sequence + line
+    f.close()
+except FileNotFoundError:
+    print(f"File: {args.filename} does not exist")
+    sys.exit()
+except IOError:
+    print(f"File: {args.filename} is not accessible")
+    sys.exit()
+
+
