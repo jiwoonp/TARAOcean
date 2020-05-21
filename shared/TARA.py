fe5b14af6acdf7c9104c1bbc6460ef0609cdb1c1
Alexander Islas <alexanderislas8@gmail.com>
2020-05-21 11:22:52 -0700

    Update TARA.py

--- shared/TARA.py
+++ shared/TARA.py
@@ -1,3 +1,12 @@
-def OGDRequest(sequence):
+def OGAsubmit(requestName, sequence):
     import subprocess
-    
+    cmd = './shared/OGAsubmit.sh'
+    run_cmd = [cmd, requestName, sequence]
+    proc = subprocess.run(run_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(50*'=')
+    print('*** screen output from worker:\n')
+    print(proc.stdout.decode())
+    print(50*'=')
+    print('*** error messages from worker:\n')
+    print(proc.stderr.decode())
+    print(50*'=')
