db4ccf138c7f47e5b29aaec69da0ce401ad0de82
Alexander Islas <alexanderislas8@gmail.com>
2020-05-28 10:41:22 -0700

    Update dataManipulation.py
    Access data from nested folders and unzip files needed.

--- dataManipulation.py
+++ dataManipulation.py
@@ -15,11 +15,27 @@ main_dir = os.path.abspath('.').split('/')[-1]
 input_dir = f"../{main_dir}_input/"
 output_dir = f"../{main_dir}_output/"
 raw_dir = f"{input_dir}raw/"
+rawData_dir = f"{raw_dir}request/"
+rawUnzip_dir = f"{raw_dir}unzip/"
+directory.make_dir(rawUnzip_dir)
 
 # Find each instance of data. So that you could run a big batch job.
-rawData_dirs = glob.glob(f"{raw_dir}*")
+rawData_dirs = glob.glob(f"{rawData_dir}*")
 
-for rawData_dir in rawData_dirs:
-    zip = ZipFile(rawData_dir+'/abun_enviro_data.zip','r')
-    # TO DO: Extract .csv files. Could put them to a separate folder.
+for rawRequest_dir in rawData_dirs:
+    # Verify that zip files are closed
+    try: zip.close()
+    except: pass
+    zip = ZipFile(rawRequest_dir+'/abun_enviro_data.zip','r')
+    zip.extractall(rawUnzip_dir)
+    # Clean up zip files
+    zip.close()
+    
+
+
+
+
+
+    # TO DO: 
     # Then do all manipulation within this FOR loop
+    # Clean up. zip raw request data. delete unzip folder. save output to pickle
