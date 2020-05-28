8e5b5eed2e8d4cc157f33cf409559764ee904e67
delasislas <alexanderislas8@gmail.com>
2020-05-28 12:32:12 -0700

    Add the data parsing from Jiwoon.

--- dataManipulation.py
+++ dataManipulation.py
@@ -35,9 +35,40 @@ for rawRequest_dir in rawData_dirs:
     # Parse environmental data
     env_par = pd.read_csv(f'{rawUnzip_dir}environmental_parameters.csv', sep = '\t')
     gene_ab = pd.read_csv(f'{rawUnzip_dir}abundance_matrix.csv', sep = '\t', skiprows = [1])
+    gene_ab.insert(0, 'ID', range(1, gene_ab.shape[0]+1))       # assign ID to each row 
+
+    taxonomy = gene_ab['Unnamed: 1'].str.split('; ',expand=True)
+    #gene_ab = pd.concat([taxonomy, gene_ab], axis=1)
+    #gene_ab = gene_ab.dropna(axis=0, subset=[1])  
+    m = taxonomy[1] != 'Bacteria'
+    gene_ab_euk, gene_ab_prok = taxonomy[m], taxonomy[~m]       # needed to split data to prokaryotes and non-prokaryotes due to different taxonomic levels
+
+    gene_ab_prok = gene_ab_prok[gene_ab_prok.columns[0:8]]
+    gene_ab_prok.columns = ['Biota', 'Superkingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genera', 'Species']
+    gene_ab_euk = gene_ab_euk.dropna(axis=0, subset=[0])
+    gene_ab_euk.columns = ['Biota', 'Superkingdom', 'Kingdom', 'Superphylum', 'Phylum', 'Subphylum', 'Class', 'Subclass', 'Infraclass',
+                 'Cohort', 'Order', 'Suborder', 'Infraorder', 'Superfamily', 'Family', 'Subfamily', 'Tribe', 'Genus']
+
+    gene_ab = pd.concat([gene_ab, pd.concat([gene_ab_prok, gene_ab_euk])], axis=1)
+    # calculating sum of abundance for each taxonomic level
+    gene_ab_melt = gene_ab.melt(id_vars = ['ID', 'Biota', 'Superkingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genera', 'Species', 'Kingdom', 'Superphylum', 'Subphylum', 'Subclass', 'Infraclass', 'Cohort', 'Suborder', 'Infraorder', 'Superfamily', 'Subfamily', 'Tribe', 'Genus'])
+    dummy = pd.get_dummies(gene_ab_melt, columns = ['Biota', 'Superkingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genera', 'Species', 'Kingdom', 'Superphylum', 'Subphylum', 'Subclass', 'Infraclass', 'Cohort', 'Suborder', 'Infraorder', 'Superfamily', 'Subfamily', 'Tribe', 'Genus'], dtype = 'int')
+    for col in dummy.columns.to_list():
+        if col != ['value'] and dummy[col].dtype == 'int':
+            dummy[col] = np.where(dummy[col] == 1, dummy['value'], dummy[col])
+    gene_ab_final = dummy.groupby('variable').sum().reset_index().rename({'variable' : 'rownames'}, axis=1).drop('value', axis=1)
+
+    # merged environmental parameters and gene abundance into one file
+    alldata = pd.merge(left=env_par, right=gene_ab_final, how='left', left_on='OGA_ID.1', right_on='rownames')
     
+    # Save merged data to pickle
+    req = rawRequest_dir.split('/')[-1]
+    alldata.to_pickle(f'{output_dir}{req}dataProc.p')
+    # Zip input folder
+    try: zip.close()
+    except: pass
+
 
 
     # TO DO: 
-    # Then do all manipulation within this FOR loop
     # Clean up. zip raw request data. delete unzip folder. save output to pickle
